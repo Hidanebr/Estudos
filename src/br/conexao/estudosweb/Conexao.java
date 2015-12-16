7253e0073835f54dbb641d7e7d2572fc6cb610dc
kleber Santos <kronos835@gmail.com>
2015-12-15 22:28:54 -0300

    BR_CRODRIGUES_C00006_Master_Inserindo_Conex_Fachada

---
+++ src/br/conexao/estudosweb/Conexao.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.conexao.estudosweb;
+
+/**
+ * @author kleber
+ *
+ */
+public class Conexao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/br/fachada/estudosweb/Fachada.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.fachada.estudosweb;
+
+/**
+ * @author kleber
+ *
+ */
+public class Fachada {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
